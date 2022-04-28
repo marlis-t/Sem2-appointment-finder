4bfda2568d4d96a8d64476efe518f0bd1d84150d
marlis-t <if21b152@technikum-wien.at>
2022-04-28 18:36:23 +0200

    final edits

--- backend/db/db_access.php
+++ backend/db/db_access.php
@@ -1,7 +1,7 @@
 <?php
 //all information necessary for connecting to database
 $db_servername = "localhost";
-$db_username = "root";
-$db_password = "";
+$db_username = "bif2webscriptinguser";
+$db_password = "bif2021";
 $db_name = "websem2_db";
 ?>
